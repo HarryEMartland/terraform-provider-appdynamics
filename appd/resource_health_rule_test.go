9d5a9fdfef53720ed55344a182218718e960e659
martlandh <HarryEMartland@gmail.com>
2020-07-31 19:03:56 +0100

    adds update single metric health rule acceptance test

--- appd/resource_health_rule_test.go
+++ appd/resource_health_rule_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-func TestAccAppDHealthRule_basicSMS(t *testing.T) {
+func TestAccAppDHealthRule_basicSingleMetricAllBts(t *testing.T) {
 
 	name := acctest.RandStringFromCharSet(11, acctest.CharSetAlphaNum)
 
@@ -40,6 +40,73 @@ func TestAccAppDHealthRule_basicSMS(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "compare_condition", condition),
 					resource.TestCheckResourceAttr(resourceName, "warn_compare_value", warnValue),
 					resource.TestCheckResourceAttr(resourceName, "critical_compare_value", criticalValue),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					CheckHealthRuleExists(resourceName),
+				),
+			},
+		},
+		CheckDestroy: CheckHealthRuleDoesNotExist(resourceName),
+	})
+}
+
+func TestAccAppDHealthRule_updateSingleMetricAllBts(t *testing.T) {
+
+	name := acctest.RandStringFromCharSet(11, acctest.CharSetAlphaNum)
+
+	resourceName := "appd_health_rule.test_all_bts"
+	aggregationFunction := "VALUE"
+	detailType := "SINGLE_METRIC"
+	entityType := "BUSINESS_TRANSACTION_PERFORMANCE"
+	metric := "95th Percentile Response Time (ms)"
+	condition := "GREATER_THAN_SPECIFIC_VALUE"
+	warnValue := "1"
+	criticalValue := "2"
+
+	updatedAggregationFunction := "SUM"
+	updatedMetric := "95th Percentile Response Time (ms)"
+	updatedCondition := "LESS_THAN_SPECIFIC_VALUE"
+	updatedWarnValue := "3"
+	updatedCriticalValue := "4"
+
+	resource.Test(t, resource.TestCase{
+		Providers: map[string]terraform.ResourceProvider{
+			"appd": Provider(),
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: allBTsHealthRule(name, aggregationFunction, detailType, entityType, metric, condition, warnValue, criticalValue),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "application_id", applicationIdS),
+					resource.TestCheckResourceAttr(resourceName, "metric_aggregation_function", aggregationFunction),
+					resource.TestCheckResourceAttr(resourceName, "eval_detail_type", detailType),
+					resource.TestCheckResourceAttr(resourceName, "affected_entity_type", entityType),
+					resource.TestCheckResourceAttr(resourceName, "business_transaction_scope", "ALL_BUSINESS_TRANSACTIONS"),
+					resource.TestCheckResourceAttr(resourceName, "metric_eval_detail_type", "SPECIFIC_TYPE"), //bug in api?
+					resource.TestCheckResourceAttr(resourceName, "metric_path", metric),
+					resource.TestCheckResourceAttr(resourceName, "compare_condition", condition),
+					resource.TestCheckResourceAttr(resourceName, "warn_compare_value", warnValue),
+					resource.TestCheckResourceAttr(resourceName, "critical_compare_value", criticalValue),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					CheckHealthRuleExists(resourceName),
+				),
+			},
+			{
+				Config: allBTsHealthRule(name, updatedAggregationFunction, detailType, entityType, updatedMetric, updatedCondition, updatedWarnValue, updatedCriticalValue),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "application_id", applicationIdS),
+					resource.TestCheckResourceAttr(resourceName, "metric_aggregation_function", updatedAggregationFunction),
+					resource.TestCheckResourceAttr(resourceName, "eval_detail_type", detailType),
+					resource.TestCheckResourceAttr(resourceName, "affected_entity_type", entityType),
+					resource.TestCheckResourceAttr(resourceName, "business_transaction_scope", "ALL_BUSINESS_TRANSACTIONS"),
+					resource.TestCheckResourceAttr(resourceName, "metric_eval_detail_type", "SPECIFIC_TYPE"), //bug in api?
+					resource.TestCheckResourceAttr(resourceName, "metric_path", updatedMetric),
+					resource.TestCheckResourceAttr(resourceName, "compare_condition", updatedCondition),
+					resource.TestCheckResourceAttr(resourceName, "warn_compare_value", updatedWarnValue),
+					resource.TestCheckResourceAttr(resourceName, "critical_compare_value", updatedCriticalValue),
 					resource.TestCheckResourceAttr(resourceName, "name", name),
 					resource.TestCheckResourceAttrSet(resourceName, "id"),
 					CheckHealthRuleExists(resourceName),
